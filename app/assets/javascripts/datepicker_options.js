d23cc1d0246b93a88e1104f8b1749805e959146b
Eshwar Chandra <eshwarcc@gmail.com>
2012-08-15 18:39:13 +0530

    Made changes to popups, rendered document info as read-only

--- app/assets/javascripts/datepicker_options.js
+++ app/assets/javascripts/datepicker_options.js
@@ -3,7 +3,7 @@ $(function() {
 	$.datepicker.setDefaults({
 		changeYear: true,
 		yearRange: "-100:c",
-		// dateFormat: "yy-mm-dd",
+		dateFormat: "yy-mm-dd",
 		// showOn: 'both',
 		// buttonImageOnly: true,
 	});
