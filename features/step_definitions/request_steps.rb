50b8bcd2622875a1455956864f92f79ccb14ac37
Fathima <fathima@my-pc.(none)>
2012-10-01 13:12:45 +0530

    updated cucumber scripts for Request

--- features/step_definitions/request_steps.rb
+++ features/step_definitions/request_steps.rb
@@ -1,9 +1,12 @@
-When /^I click on "(.*?)"$/ do |link|
-  click_link link
-end
+#When /^I click on "(.*?)"$/ do |link|
+ # click_link link
+#end
 
-Then /^I click Edit inside "(.*?)"$/ do |row|
-	within "##{row}" do
-		step %{I click on "Edit"}
-	end
+#Then /^I click Edit inside "(.*?)"$/ do |row|
+#	within "##{row}" do
+#		step %{I click on "Edit"}
+#	end
+#end
+When /^I confirm Popup$/ do
+  page.driver.browser.switch_to.alert.accept    
 end
